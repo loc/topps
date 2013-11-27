f52b5e7387dd4d1a43723024d7d02f0e32accec9
zverham <zdv8rb@virginia.edu>
2013-11-26 20:23:37 -0500

    buying packs

--- topps/stmts/stmts.py
+++ topps/stmts/stmts.py
@@ -22,11 +22,19 @@ get_pack = """SELECT * FROM packs
 
 # (user_id, pack_id)
 purchase_pack = """INSERT INTO card
-                       SELECT (NULL, %1$s, packs_players.player_id)
-                       FROM packs
-                            INNER JOIN packs_players ON packs.pack_id = packs_players.pack_id
-                       WHERE packs.pack_id = %2$s
-                             AND (SELECT users.points FROM users WHERE users.id = %1$s) > packs.points; 
-                   UPDATE users 
-                          SET users.points = users.points - (SELECT packs.points FROM packs WHERE packs.id = %2$s) 
-                          WHERE users.id = %1$s;"""
+SELECT NULL, packs_players.player_id, %(user_id)s
+FROM packs
+INNER JOIN packs_players ON packs.pack_id = packs_players.pack_id
+WHERE packs.pack_id = %(pack_id)s
+AND (SELECT users.points FROM users WHERE users.id = %(user_id)s) > packs.points;"""
+
+#(user_id, pack_id)
+deduct_points = """UPDATE users
+SET users.points = users.points - (SELECT packs.points FROM packs WHERE packs.pack_id = %(pack_id)s )
+WHERE users.id = %(user_id)s;"""
+
+#(user_id,)
+get_user_points = """SELECT points FROM users WHERE id=%(user_id)s;"""
+
+#(pack_id,)
+get_pack_points = """SELECT points from packs WHERE pack_id=%(pack_id)s;"""
--- topps/views.py
+++ topps/views.py
@@ -53,3 +53,25 @@ def logout():
         return redirect(url_for('login'))
     session.pop("user")
     return redirect(url_for('index'))
+
+@app.route('/purchase/<pack_id>', methods=['GET', 'POST'])
+def purchase(pack_id):
+	cur = g.db.cursor()
+	if request.method == 'POST':
+		print session['user']
+		print pack_id
+		cur.execute(sql.purchase_pack, {"user_id": session['user'], "pack_id": pack_id})
+		cur.execute(sql.get_user_points, {"user_id": session['user']})
+		user_points = cur.fetchone()
+		cur.execute(sql.get_pack_points, {"pack_id": pack_id})
+		pack_points = cur.fetchone()
+		if user_points >= pack_points:
+			cur.execute(sql.deduct_points, {"user_id": session['user'], "pack_id": pack_id})
+			g.db.commit()
+		else:
+			return render_template("status.html", status_text="you dont have enough pts")		
+		return render_template("status.html", status_text="trade complete")		
+	cur.execute(sql.get_pack, (pack_id,))
+	results = cur.fetchall()
+	#print results
+	return render_template("purchase_pack.html", results=results)
