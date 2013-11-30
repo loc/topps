82a571ab42ca13e26976e25e3e667407a706f41b
Sam Elliott <sam@lenary.co.uk>
2013-11-29 19:46:29 -0500

    2 mysql users

--- topps/__init__.py
+++ topps/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
 
-app.config.from_object('topps.config.db')
+app = Flask(__name__)
+app.config.from_object('topps.config.db_app')
+
+scraper = Flask(__name__)
+scraper.config.from_object('topps.config.db_scraper')
 
 import topps.views
